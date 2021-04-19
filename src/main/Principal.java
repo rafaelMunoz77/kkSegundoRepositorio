ec84c001daebb6ce445da83ce75554f8346431eb
Rafael Munoz <rafael.munoz77@gmail.com>
2021-04-19 13:40:12 +0200

    primer commit

---
+++ src/main/Principal.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+	}
+
+}
